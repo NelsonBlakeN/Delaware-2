b65e87ec4bc08353241416d5ad07ba2219f9cbe1
Blake Nelson <blake@localhost.localdomain>
2018-05-02 18:39:10 -0500

    Rename file, add function headers

--- main.py
+++ main.py
@@ -12,7 +12,6 @@
 '''''''''''''''''''''''''''
 import sys
 
-# IMPROVEMENT: Error handling
 try:
     import DbConnector
     import serial
@@ -25,26 +24,26 @@ except Exception as e:
 
 TIME_INTERVAL = 60
 
-#-----------------------------------------
-# Name: main
-# PreCondition:  None
-# PostCondition: Database entries will exist, with accurate information
-#                about car occupancy in a given parking lot.
-#-----------------------------------------
 class Main:
+    #-----------------------------------------
+    # Name: __init__
+    # PreCondition:  None
+    # PostCondition: A main object will be instantiated,
+    #                which will be used to drive the database
+    #                interaction.
+    #-----------------------------------------
     def __init__(self):
-
         # Define constants
-        DEVPORT = '/dev/ttyACM0'    # Serial port used by Arduino on host machine
+        DEVPORT = '/dev/ttyACM0'                            # Serial port used by Arduino on host machine
         BAUDRATE = 115200
         DATABASE = "blake.nelson"
         USER = "blake.nelson"
         PASSWD = "Tamu@2019"
-        self.LOCATION = "Lot 35"         # Parking lot location
+        self.LOCATION = input("Enter the parking lot: ")    # Parking lot location
 
         self.times = []
         self.board = serial.Serial(DEVPORT, BAUDRATE)
-        self.debug = False# (input("Debug mode? (y/n) ") == "y")
+        self.debug = False
         if not self.debug:
             self.DbConnector = DbConnector.DbConnector(
             DATABASE,
@@ -53,7 +52,14 @@ class Main:
             )
         print("Initialization complete")
 
-    def run(self):
+    #-----------------------------------------
+    # Name:          Run
+    # PreCondition:  None
+    # PostCondition: The serial port will have been
+    #                read for all values passed by the hardware,
+    #                and pushed to the database.
+    #-----------------------------------------
+    def Run(self):
         self.running = True
         try:
             if not self.debug:
@@ -74,13 +80,21 @@ class Main:
                         data = self.board.read()
                         print(str(now) + ": " + str(data))
                         if data:
-                            self.addTimeStamp(data, now)
+                            self.AddTimeStamp(data, now)
 
         except Exception as e:
             print("ERROR Python setup failed: {}".format(e))
             sys.exit()
 
-    def sendData(self):
+    #-----------------------------------------
+    # Name:          SendData
+    # PreCondition:  None
+    # PostCondition: An asynchronous thread will have
+    #                pushed the buffered data to the
+    #                database, and a new thread will
+    #                be created.
+    #-----------------------------------------
+    def SendData(self):
         count = len(self.times)
         if(count > 0):
             print("Uploading data...")
@@ -93,9 +107,15 @@ class Main:
         else:
             print("No data, skipping upload")
         # Run it again in a bit
-        Timer(TIME_INTERVAL, self.sendData).start()
+        Timer(TIME_INTERVAL, self.SendData).start()
 
-    def addTimeStamp(self, direction, timeStamp):
+    #-----------------------------------------
+    # Name:          AddTimeStamp
+    # PreCondition:  None
+    # PostCondition: A timestamp of data read will
+    #                be buffered for database insertion.
+    #-----------------------------------------
+    def AddTimeStamp(self, direction, timeStamp):
         if self.debug:
             pass
         else:
@@ -104,4 +124,4 @@ class Main:
 
 if __name__ == "__main__":
     m = Main()
-    m.run()
+    m.Run()
